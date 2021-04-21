f436ecfd2073f800e2eccf3d6da0214d762b6b66
Pratik <pratik1616@pratik-1616>
2021-04-21 15:52:20 +0530

    Rendering a html file

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const http = require('http');
 const port = 8000;
 
+// importing fs module to read html files
+const fs = require('fs');
+
 function requestHandler(req,res){
     console.log(req.url);
-    
-    // 200 is the response status code for successful requests
     res.writeHead(200, {'content-type': 'text/html'})
-    res.end('<h1>Gotcha!</h1>');
+
+    // readFile function reads the html file, data contains the contents of the html file
+    fs.readFile('./index.html', function(err, data){
+        if(err){
+            console.log('error', err);
+            return res.end('<h1>There is an error!</h1>')
+        }
+        return res.end(data);
+    });
 }
 
 const server = http.createServer(requestHandler);
