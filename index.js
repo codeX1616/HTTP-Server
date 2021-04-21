e7380b03d7c1ba59bcf6a568a48d1c6e60e75765
Pratik <pratik1616@pratik-1616>
2021-04-21 16:38:53 +0530

    Rendering html files to different request urls

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const http = require('http');
 const port = 8000;
-
-// importing fs module to read html files
 const fs = require('fs');
 
 function requestHandler(req,res){
     console.log(req.url);
-    res.writeHead(200, {'content-type': 'text/html'})
+    res.writeHead(200, {'content-type': 'text/html'});
 
-    // readFile function reads the html file, data contains the contents of the html file
-    fs.readFile('./index.html', function(err, data){
+    // filePath stores which html file to open
+    let filePath;
+    switch(req.url){
+        case '/':
+            filePath = './index.html'
+            break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+        default:
+            filePath = './error.html'
+    }
+
+    // filePath is passed as argument to readFile function
+    fs.readFile(filePath, function(err, data){
         if(err){
-            console.log('error', err);
-            return res.end('<h1>There is an error!</h1>')
+            console.log(err);
+            return res.end('<h1>Error!</h1>')
         }
         return res.end(data);
-    });
+    })
+
 }
 
 const server = http.createServer(requestHandler);
